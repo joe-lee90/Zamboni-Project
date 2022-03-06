437b3a2e0a5df6161bd040cf5a9ca012e7b68b3a
Joe <zvmboni@gmail.com>
2022-03-05 21:04:03 -0800

    implement js functionality

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const h2 = document.createElement("h2");
-document.querySelector("body").appendChild(h2);
+function addingEventListener(){
+    const image = document.getElementById('imageZamboni');
+    image.addEventListener('click', function() {
+        alert("I was clicked!");
+    })
+}
+function clickZamboni(){
+    const image = document.getElementById('imageZamboni');
+    alert("Why Zamboni? \nIt's been my nickname since I was a kid and it also serves as my superhuman alter-ego that I invoke in special situations");
+}
